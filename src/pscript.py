eb908ab099f7e7db5f2024883eae17efdf0d68ba
Gianman <gsarantidis217@gmail.com>
2018-09-16 13:30:32 +0300

    Restructure; Introduce main function

--- src/pscript.py
+++ src/pscript.py
@@ -1,8 +1,24 @@
-import os
+
+user_nums = list()
+
 
 def funct(a, b):
     return a + b
 
-result = funct(1, 2)
-print('Result is {}'.format(result))
 
+def prompt():
+    a = raw_input('Enter a number: ')
+    return int(a)
+
+
+def main():
+    for i in range(0, 2):
+        temp = prompt()
+        user_nums.append(temp)
+
+    result = funct(user_nums[0], user_nums[1])
+    print('Result is {}'.format(result))
+
+
+if __name__ == '__main__':
+    main()
